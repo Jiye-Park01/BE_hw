42ac3d464c7075bab4d2b7102c323cb2458b4faa
Jiye-Park01 <jiye0710@naver.com>
2024-03-27 17:23:39 +0900

    Django 기초 과제

---
+++ wordCount/wordCount_Pro/wordCountApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def wordCount(request):
+    return render(request, "wordCount.html")
+def result(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+
+    word_dictionary = {}
+    
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+    return render(request, "result.html", {'alltext': entered_text, 'dictionary': word_dictionary.items(), 'count':len(word_list), 'word_num':len(entered_text), 'word_except':len(entered_text)-len(word_list)+1})
+
+def hello(request):
+    entered_name = request.GET['getName']
+    return render(request, "hello.html", {'name_text': entered_name})
---
+++ wordCount/wordCount_Pro/wordCount_Pro/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for wordCount_Pro project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from wordCountApp import views         #urls.py에서 wordCountApp의 views.py를 사용하겠따는 의미
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name="index"),        #최상단 경로에 유저가 접속하면 views.py에 있는 index 함수 실행하라는 의미
+    path('wordCount/', views.wordCount, name="wordCount"),
+    path('result/', views.result, name="result"), 
+    path('hello/', views.hello, name="hello"), 
+]
